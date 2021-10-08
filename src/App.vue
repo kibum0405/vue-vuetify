f03937892e93a882bc613765d558a44e35609b20
kibumpark <top5w5@naver.com>
2021-10-08 20:42:54 +0900

    vue header 내용 변경

--- src/App.vue
+++ src/App.vue
@@ -1,40 +1,31 @@
 <template>
   <v-app>
+      <!-- header  -->
     <v-app-bar
       app
       color="primary"
       dark
     >
-      <div class="d-flex align-center">
-        <v-img
-          alt="Vuetify Logo"
-          class="shrink mr-2"
-          contain
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-logo-dark.png"
-          transition="scale-transition"
-          width="40"
-        />
+        <v-btn text href="/ " class="d-flex align-center">
+            <v-img
+            alt="Vuetify Logo"
+            class="shrink mr-2"
+            contain
+            src="https://cdn.vuetifyjs.com/images/logos/vuetify-logo-dark.png"
+            transition="scale-transition"
+            width="40"/>
+            <h1 style="font-weight:500;">VuetifyStudy</h1>
+        </v-btn>
 
-        <v-img
-          alt="Vuetify Name"
-          class="shrink mt-1 hidden-sm-and-down"
-          contain
-          min-width="100"
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-name-dark.png"
-          width="100"
-        />
-      </div>
+        <v-spacer></v-spacer>
 
-      <v-spacer></v-spacer>
-
-      <v-btn
-        href="https://github.com/vuetifyjs/vuetify/releases/latest"
-        target="_blank"
-        text
-      >
-        <span class="mr-2">Latest Release</span>
-        <v-icon>mdi-open-in-new</v-icon>
-      </v-btn>
+        <v-btn
+            href="https://github.com/kibum0405/vue-vuetify"
+            target="_blank"
+            text>
+            <span class="mr-2">Github</span>
+            <v-icon>mdi-git</v-icon>
+        </v-btn>
     </v-app-bar>
 
     <v-main>
@@ -47,14 +38,18 @@
 import HelloWorld from './components/HelloWorld';
 
 export default {
-  name: 'App',
+    name: 'App',
 
-  components: {
-    HelloWorld,
-  },
+    components: {
+        HelloWorld,
+    },
 
-  data: () => ({
-    //
-  }),
+    methods: {
+    },
+
+
+    data: () => ({
+        //
+    }),
 };
 </script>
