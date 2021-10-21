53ef16e75942e2d3f3e8e344b7d37d73993b11eb
kibumpark <top5w5@naver.com>
2021-10-21 20:31:15 +0900

    탭 생성

--- src/Home.vue
+++ src/Home.vue
@@ -6,7 +6,7 @@
                 color="primary"
                 dark
             >
-                <v-btn text href="/ " class="d-flex align-center">
+                <v-btn text href="/" class="d-flex align-center">
                     <v-img
                     alt="Vuetify Logo"
                     class="shrink mr-2"
@@ -31,31 +31,55 @@
       <!-- header  -->
     
 <!-- 투두리스트 영역 -->
-<div>
-    <v-main>
-        <div style = "text-align:center;">
-            <h1>할일 목록</h1>
-            <p>전체 할일: {{ todoList.length }}/ 완료돤 할일: / 남은 할일: </p>
-        </div>
-        <v-row>
-            <v-col cols="6">
-                <List
-                    :todoList="todoList"
-                    @statusControl="statusControl"
-                    @listDelete="listDelete"
-                />
-            </v-col>
+    <div>
+        <v-main>
+            <v-tabs
+                v-model="tab"
+                background-color="transparent"
+                color="basil"
+                grow
+                >
+                <v-tab
+                    v-for="item in items"
+                    :key="item"
+                >
+                    {{ item }}
+                </v-tab>
+            </v-tabs>
+            <v-tabs-items v-model="tab">
+                <v-tab-item v-for="item in items"
+                            :key="item"
+                >
+                    <div v-if="tab === 0">
+                        <div style = "text-align:center;">
+                            <h1>할일 목록</h1>
+                            <p>전체 할일: {{ todoList.length }}/ 완료돤 할일:{{ countDone }} / 남은 할일:{{ todoList.length - countDone }} </p>
+                        </div>
+                        <v-row>
+                            <v-col cols="6">
+                                <List
+                                    :todoList="todoList"
+                                    @statusControl="statusControl"
+                                    @listDelete="listDelete"
+                                />
+                            </v-col>
 
-            <v-col cols="6">
-                <ListAdd 
-                    @listAdd="listAdd"
-                    @listEdit="listEdit"
-                />
-            </v-col>
-        </v-row>
-    </v-main>
-</div>
-    
+                            <v-col cols="6">
+                                <ListAdd 
+                                    @listAdd="listAdd"
+                                    @listEdit="listEdit"
+                                />
+                            </v-col>
+                        </v-row>
+                    </div>
+
+                    <div v-if="tab === 1">
+                        <div style = "width:100px; height:100px; background-color:red;"></div>
+                    </div>
+                </v-tab-item>
+            </v-tabs-items>
+        </v-main>
+    </div>
 <!-- 투두리스트 영역 끝 -->
   </v-app>
 </template>
@@ -73,14 +97,25 @@ export default {
     data() {
         return {
             todoList: [],
+            tab: null,
+            items: [
+                'TodoList', 'Routes', 'Deserts', 'Cocktails',
+            ],
         }
     },
     computed: {
-
+        countDone() {
+            let count = 0
+            this.todoList.forEach(list => {
+                if (list.status === 'done') count++
+            })
+            return count
+        }
     },
     methods: {
         listAdd(memo){
             this.todoList.push({ memo: memo, status: "created" })
+            console.log(memo)
         },
         statusControl(index, status){
             this.todoList[index].status = status
